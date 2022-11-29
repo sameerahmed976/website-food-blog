157755f56bbdb7ef9b603da9e6a0453d521961a3
mohammed sameer ahmed <sameerahmed976@gmail.com>
2022-11-29 12:04:22 +0530

    added navbar for mobile

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import "./style.css";
 
 const menuElement = document.querySelector(".menu");
+const NavLinksElement = document.querySelector(".nav__links");
 
 menuElement.addEventListener("click", () => {
   menuElement.classList.toggle("show");
+  NavLinksElement.classList.toggle("nav__links--show");
   //   console.log("click");
 });
